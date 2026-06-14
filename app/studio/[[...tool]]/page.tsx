1f8d21669d51b5c9e301352cbc747cbbe48e48c3
R8Turbo77 <r8turbo77@gmail.com>
2026-06-15 00:39:12 +0100

    Load Sanity Studio on client

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,10 @@
-import { NextStudio } from 'next-sanity/studio';
-import config from '@/sanity.config';
+import { StudioClient } from './StudioClient';
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
+export const metadata = {
+  title: 'MA Motors Admin Studio'
+};
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return <StudioClient />;
 }
